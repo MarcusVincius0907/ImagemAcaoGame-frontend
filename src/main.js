616387da89a33da475d995066aa71bc0a823df80
Marcus0907 <mvleite0908@gmail.com>
2021-09-16 22:03:10 -0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+//import Vuex from 'vuex'
+
+const app = createApp(App)
+
+//app.use(Vuex);
+//import store from './store/index'
+//app.config.globalProperties.$store = store
+
+app.mount('#app');
